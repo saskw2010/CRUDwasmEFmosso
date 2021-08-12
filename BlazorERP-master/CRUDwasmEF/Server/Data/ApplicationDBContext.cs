ab49b17931a65f161774f8218889d3449d4202dc
mostafa elnagar <m.nagar@whitesky.tech>
2021-08-12 13:09:40 +0300

    Add project files.

---
+++ BlazorERP-master/CRUDwasmEF/Server/Data/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRUDwasmEF.Shared.Models;
+
+namespace CRUDwasmEF.Server.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Developer> Developers { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ CRUDwasmEF/Shared/Models/Developer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDwasmEF.Shared.Models
+{
+    public class Developer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")] public decimal Experience { get; set; } 
+    }
+}
